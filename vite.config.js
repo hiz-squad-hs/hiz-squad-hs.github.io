b2ee40fb3409a4ece36a4d254d51bf989e94bba6
hizsquad <kuznecovmatvej717@gmail.com>
2026-06-13 09:23:06 +0500

    v0.1.0.1

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,12 @@ import { defineConfig } from 'vite';
 import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig({
-  base: '/hizsite/', // Обязательно со слэшами по бокам!
+  base: '/hizsite/', // Твой путь на Гитхабе
   plugins: [injectHTML()],
+  build: {
+    outDir: '.', // Собираем сайт прямо в корень, а не в папку dist!
+    emptyOutDir: false // Чтобы Vite случайно не стёр твои исходники при сборке
+  },
   server: {
     allowedHosts: true
   }
