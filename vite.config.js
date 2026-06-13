d3a7fbdf6a197b38697ed9bbef88cffdc54a5f71
hizsquad <kuznecovmatvej717@gmail.com>
2026-06-13 09:36:47 +0500

    v0.1.0.4

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite';
 import injectHTML from 'vite-plugin-html-inject';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/hizsite/', 
-  plugins: [injectHTML()], // Исправили имя плагина
+  plugins: [injectHTML()],
   build: {
-    outDir: 'docs', // Собираем в безопасную папку docs
+    outDir: 'docs',
+    // Говорим Vite собирать обе страницы, чтобы они долетали до Гитхаба
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        notfound: resolve(__dirname, '404.html'),
+        page2: resolve(__dirname, 'page2.html') // если есть вторая страница
+      }
+    }
   },
   server: {
     allowedHosts: true
